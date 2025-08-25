30571bd3d46ffe0e85349374b5f5227bf5f85965
Santiago Tabares <satabaresmo@gmail.com>
2025-08-24 21:17:22 -0500

    chore(scaffold): generate models, use cases, adapters and entry points

---
+++ domain/model/src/main/java/co/com/pragma/model/loanstate/gateways/LoanStateRepository.java
@@ -0,0 +1,4 @@
+package co.com.pragma.model.loanstate.gateways;
+
+public interface LoanStateRepository {
+}
---
+++ domain/usecase/src/main/java/co/com/pragma/usecase/loanapplication/LoanApplicationUseCase.java
@@ -0,0 +1,6 @@
+package co.com.pragma.usecase.loanapplication;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class LoanApplicationUseCase {
+}
