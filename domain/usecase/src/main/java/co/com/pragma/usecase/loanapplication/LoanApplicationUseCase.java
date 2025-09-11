bad5a4800a4e41e3424b7c600a7110919971c3dd
Santiago Tabares <satabaresmo@gmail.com>
2025-09-10 21:02:32 -0500

    feat(loan-application): add unit tests for automatic review

--- domain/usecase/src/main/java/co/com/pragma/usecase/loanapplication/LoanApplicationUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/usecase/loanapplication/LoanApplicationUseCase.java
@@ -309,12 +309,6 @@ public class LoanApplicationUseCase implements ILoanApplicationUseCase  {
                 ));
     }
 
-    @Override
-    public Flux<LoanBasicInfo> getApprovedLoanInfoByUserDocument(String document) {
-        logger.info("Fetching loan application by id: " + document);
-        return loanApplicationRepository.getApprovedLoansByDocument(document)
-                .doOnComplete(() -> logger.info("Finished fetching approved  loan applications"));
-    }
 
     // Construye el informe combinando la información de préstamos y usuarios.
     private LoanUserReport buildLoanUserReport(List<LoanInfo> loans, List<UserInfo> users) {
@@ -394,6 +388,12 @@ public class LoanApplicationUseCase implements ILoanApplicationUseCase  {
     }
 
 
+    @Override
+    public Flux<LoanBasicInfo> getApprovedLoanInfoByUserDocument(String document) {
+        logger.info("Fetching loan application by id: " + document);
+        return loanApplicationRepository.getApprovedLoansByDocument(document)
+                .doOnComplete(() -> logger.info("Finished fetching approved  loan applications"));
+    }
 
 
     public Flux<LoanApplication> getAllLoanApplications() {
